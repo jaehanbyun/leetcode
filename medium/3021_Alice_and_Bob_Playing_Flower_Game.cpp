47c5ac408ea5bfce4842657ebf0d793b729065e0
jaehanbyun <awbrg789@naver.com>
2025-08-29 09:42:28 +0900

    solve #3021 Alice and Bob Playing Flower Game (Medium) [C++]

---
+++ medium/3021_Alice_and_Bob_Playing_Flower_Game.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    long long flowerGame(int n, int m) {
+        long long oddN = (n + 1) / 2, evenN = n / 2;
+        long long oddM = (m + 1) / 2, evenM = m / 2;
+        
+        return oddN * evenM + evenN * oddM;
+    }
+};
