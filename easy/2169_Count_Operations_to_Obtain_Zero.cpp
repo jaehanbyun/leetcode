063e8cff93ac8330623383e4bdeb4e1fd12862a4
jaehanbyun <awbrg789@naver.com>
2025-11-09 14:51:57 +0900

    solve #2169 Count Operations to Obtain Zero (Easy) [C++]

---
+++ easy/2169_Count_Operations_to_Obtain_Zero.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        if (num1 == 0 || num2 == 0) return 0;
+        
+        int cnt = 0;
+        while (num1 != 0 && num2 != 0) {
+            if (num1 < num2) swap(num1, num2);
+            cnt += num1 / num2;
+            num1 %= num2;
+        }
+
+        return cnt;
+    }
+};
