68bb2aa26a786413df7d73743dad82ce7f2bfc8b
jaehanbyun <awbrg789@naver.com>
2025-12-15 22:45:33 +0900

    solve #2110 Number of Smooth Descent Periods of a Stock (Medium) [C++]

---
+++ medium/2110_Number_of_Smooth_Descent_Periods_of_a_Stock.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices) {
+        long long len = 1;
+        long long res = 1;
+
+        for (int i = 1; i < prices.size(); ++i) {
+            if (prices[i] == prices[i-1] - 1) len++;
+            else len = 1;
+            res += len;
+        }
+        
+        return res;
+    }
+};
