273d85b3f6882e71b71b72fb28d6dff1fa006bba
jaehanbyun <awbrg789@naver.com>
2025-12-21 16:33:59 +0900

    solve #944 Delete Columns to Make Sorted (Easy) [C++]

---
+++ easy/944_Delete_Columns_to_Make_Sorted.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int res = 0;
+
+        int n = strs.size();
+        int m = strs[0].size();
+
+        for (int col = 0; col < m; ++col) {
+            for (int row = 1; row < n; ++row) {
+                if (strs[row][col] < strs[row-1][col]) {
+                    res++;
+                    break;
+                }
+            }
+        }
+
+        return res;
+    }
+};
