a06f1ff51f21101adbdcf935518e6a6a89dd58f5
jaehanbyun <awbrg789@naver.com>
2026-05-19 09:57:03 +0900

    solve #2540 Minimum Common Value (Easy) [CPP]

---
+++ easy/2540_Minimum_Common_Value.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int p1 = 0, p2 = 0;
+        int n1 = nums1.size(), n2 = nums2.size();
+
+        while (p1 < n1 && p2 < n2) {
+            if (nums1[p1] == nums2[p2]) return nums1[p1];
+            if (nums1[p1] < nums2[p2]) p1++;
+            else p2++;
+        }
+        return -1;
+    }
+};
