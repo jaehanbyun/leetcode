76945f4fcd9e320a03758a8253eef348041baba6
jaehanbyun <awbrg789@naver.com>
2026-07-07 09:06:26 +0900

    solve #3754 Concatenate Non-Zero Digits and Multiply by Sum I (Easy) [CPP]

---
+++ easy/3754_Concatenate_Non-Zero_Digits_and_Multiply_by_Sum_I.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        long long x = 0, sum = 0;
+        string s = to_string(n);
+        for (char c: s) {
+            int d = c - '0';
+            if (d != 0) {
+                x = x * 10 + d;
+                sum += d;
+            }
+        }
+        return x * sum;
+    }
+};
