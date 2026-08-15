4caa8ecdac74c08b55949e62d46be8a364658f6e
jaehanbyun <awbrg789@naver.com>
2026-08-15 16:18:03 +0900

    solve #3702 Longest Subsequence With Non-Zero Bitwise XOR (Medium) [CPP]

---
+++ medium/3702_Longest_Subsequence_With_Non-Zero_Bitwise_XOR.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int longestSubsequence(vector<int>& nums) {
+        int n = nums.size();
+        bool allZero = true;
+        int totalXor = 0;
+
+        for (int x: nums) {
+            totalXor ^= x;
+            if (x != 0) allZero = false;
+        }
+
+        if (totalXor > 0) return n;
+        return allZero ? 0 : n-1;
+    }
+};
