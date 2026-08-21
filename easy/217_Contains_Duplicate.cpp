939da90bb7595660d861c4317e12a86ca27cf26a
jaehanbyun <awbrg789@naver.com>
2026-08-21 12:32:28 +0900

    solve #217 Contains Duplicate (Easy) [CPP]

---
+++ easy/217_Contains_Duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+        for (auto x: nums) {
+            if (seen.contains(x)) return true;
+            seen.insert(x);
+        }
+        return false;
+    }
+};
