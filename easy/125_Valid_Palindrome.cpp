2bcf276380a634e105c37e75425b8e39c112984b
jaehanbyun <awbrg789@naver.com>
2026-09-07 08:44:12 +0900

    solve #125 Valid Palindrome (Easy) [CPP]

---
+++ easy/125_Valid_Palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAlphaNum(char c) {
+        return (c >= 'A' && c <= 'Z' ||
+            c >= 'a' && c <= 'z' ||
+            c >= '0' && c <= '9');
+    }
+    bool isPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+
+        while (l < r) {
+            while (l < r && !isAlphaNum(s[l])) {
+                l++;
+            }
+
+            while (l < r && !isAlphaNum(s[r])) {
+                r--;
+            }
+
+            if (tolower(s[l]) != tolower(s[r])) return false;
+
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
